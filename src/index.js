a52c483af82277bab39d9d6aa8139e48390e5c8e
Mike Loseto <losetom1998@gmail.com>
2022-08-15 20:25:04 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const url = "https://api.spoonacular.com/recipes/716429/information?apiKey=5f51aa6b837e450d9e17ee709b954584"
+// const recipeUrl = "https://api.spoonacular.com/recipes/random"
+const recipeList = document.getElementById("recipeList")
+const form = document.getElementById("abc")
 
-fetch(url)
- .then(r => r.json())
- .then(data => console.log(data))
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const value = e.target.calorie.value
+    fetch(`https://api.spoonacular.com/recipes/findByNutrients?maxCalories=${value}&apiKey=5e710ca264ed4c81a06f3bd19998d74c`)
+        .then(r => r.json())
+        .then(data => renderList(data))
+
+}) 
+
+function renderList(data) {
+    // loop here 
+    data.forEach(recipe => {
+        console.log(recipe)
+        const recipeName = recipe.title
+        const pTag = document.createElement("p")
+        pTag.textContent = recipeName
+        recipeList.append(pTag)
+    })
+}
+
+// fetch(url)
+//  .then(r => r.json())
+//  .then(data => console.log(data))
+
+
